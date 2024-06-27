4ef2f9dc1d18702774f1a12f4340163ff0df4150
pitatir <alianocte@yandex.ru>
2024-06-28 01:33:10 +0300

    Added Item, Backpack, BackpackBuilder, Generation and Alg classes

--- src/libs/genetic_algorithm.py
+++ src/libs/genetic_algorithm.py
@@ -1,1 +1,29 @@
-import genome
+from backpack import Item
+import sys
+
+
+class Alg:
+    def __init__(self, items: list, limitWeight: int):
+        self.items = items
+        self.limitAmount = len(items)
+        self.limitWeight = limitWeight
+
+    def conductGeneticAlgorithm(self):
+        pass
+
+
+def getInput():
+    print("Введите вместимость рюкзака")
+    limitWeight = int(input())
+    items = []
+    print("Введите вес и стоимость каждой вещи с новой строки")
+    for line in sys.stdin:
+        weight, cost = line.split()
+        items.append(Item(weight, cost))
+    return items, limitWeight
+
+
+if __name__ == '__main__':
+    items, limitWeight = getInput()
+    geneticAlgorithm = Alg(items, limitWeight)
+    Alg.conductGeneticAlgorithm()
--- src/libs/genome.py
+++
@@ -1,7 +0,0 @@
-class Item:
-    def __init__(self, cost: int, weight: int):
-        self.cost = cost
-        self.weight = weight
-
-    def __str__(self):
-        return f"Вещь стоит {self.cost} и весит {self.weight}"
