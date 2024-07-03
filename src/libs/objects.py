2cb447f265cce41eddbd0c9667d8309d5d74d228
Aleksandr <112807215+baldchort@users.noreply.github.com>
2024-07-03 19:19:03 +0300

    Update objects.py

--- src/libs/objects.py
+++ src/libs/objects.py
@@ -15,6 +15,11 @@ class Item:
     def __le__(self, other: 'Item') -> bool:
         return self.weight <= other.weight
 
+    def __gt__(self, other: 'Item') -> bool:
+        return self.weight > other.weight
+
+    def __ge__(self, other: 'Item') -> bool:
+        return self.weight >= other.weight
 
 class Backpack:
     def __init__(self, amountOfEachItems: list[int]):
@@ -45,11 +50,12 @@ class Backpack:
 
     def calculateFitness(self, limitWeight: int, items: list[Item]) -> None:
         sumCost = sum(items[i].cost * self.genome[i] for i in range(len(items)))
-        if self.weight <= limitWeight:
+        overload = self.weight - limitWeight
+        if overload <= 0:
             self.cost = sumCost
         else:
             penalty = (self.weight - limitWeight) / limitWeight
-            self.cost = sumCost / (1 + penalty ** 2)
+            self.cost = int(sumCost * (1 - penalty ** 2)) if overload <= max(item.weight for item in items) else 0
 
 
 class Generation:
